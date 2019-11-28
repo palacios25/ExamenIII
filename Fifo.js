bb7ec347149353fc2f4e7993d07021ae041e98b1
Fernanda Palacios <mpalacios4@ucol.mx>
2019-11-28 15:46:14 -0600

    ...

---
+++ Fifo.js
@@ -0,0 +1,16 @@
+
+function prioridadFifo(){
+        if(this.prioridad === 1){
+            this.inicio = 0;
+            this.inicio = new Nodo;
+        if (this.prioridad === 1){
+            this.inicio = this.inicio;
+            this.inicio.siguiente = new Nodo;
+            if(this.prioridad === null){
+                this.inicio = new Nodo;
+            }
+        }
+        } else {
+            this.final.siguiente = new Nodo;
+        }
+    }
